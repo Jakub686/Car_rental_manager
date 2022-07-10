ee24476ab70e15dcaa1cc489b06c3139832538c3
Jakub <jakub.bchyla@gmail.com>
2022-07-10 08:24:16 +0200

    working app

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+import console.Console;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Console console = new Console();
+
+        while (!Console.stopApp) {
+            String command = scanner.nextLine();
+            console.run(command);
+        }
+
+    }
+}
---
+++ src/main/java/console/ConsoleBasicActions.java
@@ -0,0 +1,13 @@
+package console;
+
+public interface ConsoleBasicActions {
+
+    void run(String string);
+
+    void showMainMenu();
+
+    void wrongInputValueMessage();
+
+    void exit();
+
+}
